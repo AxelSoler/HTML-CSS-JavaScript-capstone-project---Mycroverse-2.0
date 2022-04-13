775591f3294677374ef31dcea6772235e62894ff
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-13 18:22:03 -0300

    mobile menu close button

---
+++ main.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.mobileMenu');
+const options = document.querySelector('.navMenu');
+const closeX = document.querySelector('.closeX');
+const line = document.querySelector('.hamburguer')
+
+menu.addEventListener('click', () => {
+  options.classList.toggle('active');
+  closeX.classList.add('active');
+  line.classList.add('remove');
+});
